40121f12816d7cf4f689ec95e72685caed64dff0
ANSHULKOTWAL <anshulkotwal12@gmail.com>
2025-07-17 14:21:30 +0530

    skillsSection enhancements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,14 +12,9 @@ import { Footer } from "../components/Footer";
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-      {/* Theme Toggle */}
       <ThemeToggle />
-      {/* Background Effects */}
       <StarBackground />
-
-      {/* Navbar */}
       <Navbar />
-      {/* Main Content */}
       <main>
         <HeroSection />
         <AboutSection />
@@ -28,8 +23,6 @@ export const Home = () => {
         <CertificatesSection/>
         <ContactSection />
       </main>
-
-      {/* Footer */}
       <Footer />
     </div>
   );
