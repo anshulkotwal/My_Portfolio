1ae9db683a246276336bf5a2a80f30821da78e5e
ANSHULKOTWAL <anshulkotwal12@gmail.com>
2025-07-31 10:38:50 +0530

    Added loader section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import  { useState } from 'react';
 import { Navbar } from "../components/Navbar";
 import { StarBackground } from "@/components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
@@ -6,28 +7,44 @@ import { ProjectsSection } from "../components/ProjectsSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { CertificatesSection } from "../components/CertificatesSection";
 import { ExperienceSection } from "../components/ExperienceSection";
-import {AchievementsSection} from "../components/AchievementsSection.jsx"
-import {EducationSection} from "../components/EducationSection.jsx"
+import { AchievementsSection } from "../components/AchievementsSection.jsx";
+import { EducationSection } from "../components/EducationSection.jsx";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
+import { PortfolioLoader } from "../components/PortfolioLoader"; 
 
 export const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoadingComplete = () => {
+    setIsLoading(false);
+  };
+
   return (
-    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-      <StarBackground />
-      <Navbar />
-      <main>
-        <HeroSection />
-        <AboutSection />
-        <ExperienceSection/>
-        <EducationSection/>
-        <SkillsSection />
-        <ProjectsSection />
-        <AchievementsSection/>
-        <CertificatesSection/>
-        <ContactSection />
-      </main>
-      <Footer />
-    </div>
+    <>
+      {/* Show loader first */}
+      {isLoading && (
+        <PortfolioLoader onLoadingComplete={handleLoadingComplete} />
+      )}
+      
+      {!isLoading && (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+          <StarBackground />
+          <Navbar />
+          <main>
+            <HeroSection />
+            <AboutSection />
+            <ExperienceSection />
+            <EducationSection />
+            <SkillsSection />
+            <ProjectsSection />
+            <AchievementsSection />
+            <CertificatesSection />
+            <ContactSection />
+          </main>
+          <Footer />
+        </div>
+      )}
+    </>
   );
 };
